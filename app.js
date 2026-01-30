c348525b1a2bbbd1378173309c987b3a85e31246
Abdullrahman almaghrabi <73081830+abdprince@users.noreply.github.com>
2026-01-30 22:14:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -292,7 +292,6 @@ toggleMenu(){
     // add create new card
     const addCard = document.createElement('div');
     addCard.className = 'card';
-    addCard.innerHTML = `<div style="flex:1"><p class="h">إضافة جديد</p><p class="small">إنشاء عنصر جديد بسرعة</p></div><div><button class="btn" id="addNew">+</button></div>`;
     container.appendChild(addCard);
 
     this.main.appendChild(container);
